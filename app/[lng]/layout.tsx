f684968d3120660a449b44bf7dcda929a7c31414
KDF25 <karimov.damir.faridovich@gmail.com>
2025-07-31 16:07:37 +0500

    feat: analytics

--- app/[lng]/layout.tsx
+++ app/[lng]/layout.tsx
@@ -1,11 +1,10 @@
 import { IHomePageProps } from "@/types/user";
 import { dir } from "i18next";
 import { Metadata } from "next";
-import Head from "next/head";
+import Script from "next/script";
 import { ReactNode } from "react";
 import { languages } from "../i18n/settings";
 import "./../styles/global.scss";
-import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Vip-perevod",
@@ -50,25 +49,71 @@ const RootLayout: React.FC<RootLayoutProps> = ({
   return (
     <>
       <html lang={lng} dir={dir(lng)} prefix="og: http://ogp.me/ns#">
-        {/* Google Ads script */}
-        <Script
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=AW-17380143692"
-        />
-        <Script id="google-ads" strategy="afterInteractive">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'AW-17380143692');
-        `}
-        </Script>
-        
-        <Head>
+        <head>
           <link rel="icon" href="/favicon.ico" />
-        </Head>
+
+          {/* Google Tag Manager */}
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WV9ZKJ46');
+          `}
+          </Script>
+
+          {/* Google Analytics GA4 */}
+          <Script
+            strategy="afterInteractive"
+            src="https://www.googletagmanager.com/gtag/js?id=G-C8ETRKVP9H"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-C8ETRKVP9H');
+          `}
+          </Script>
+
+          {/* Google Ads */}
+          <Script
+            strategy="afterInteractive"
+            src="https://www.googletagmanager.com/gtag/js?id=AW-17380143692"
+          />
+          <Script id="google-ads" strategy="afterInteractive">
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17380143692');
+          `}
+          </Script>
+
+          {/* Google Ads Conversion Event */}
+          <Script id="google-ads-conversion" strategy="afterInteractive">
+            {`
+            gtag('event', 'conversion', {
+              'send_to': 'AW-17380143692/PSZACOWZ_vQaEMzkv99A',
+              'value': 1.0,
+              'currency': 'USD'
+            });
+          `}
+          </Script>
+        </head>
 
         <body>
+          {/* Google Tag Manager (noscript) */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-WV9ZKJ46"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
+
           <main className="main__layout">
             <div>{children}</div>
           </main>
